c9e845abb45a9f6d92ab6052de9811cc25600cb7
Yusuf <uia66677@contiwan.com>
2018-07-19 13:17:36 -0400

    for menu

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -115,7 +115,7 @@ export class DashboardPage {
     }).then((marker: Marker) => {
       marker.showInfoWindow();
     });
-    this.map = GoogleMaps.create('map_canvas', mapOptions);
+    
     
     this.map.addMarker({
       title: 'AH-South',
